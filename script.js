d2f8ff026564cb2f17a406a2b12a351bfccea004
Pizzacoder6721 <Pizzalover250@hotmail.com>
2025-10-02 19:38:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const questionNum = 1;
 const choiceBtn1 = document.getElementById("choiceButton1");
 let progressNum;
 
-choiceButton1.onclick = function(){
+choiceBtn1.onclick = function(){
   if(questionNum == 1){
     progressNum = 1;
-    progressNum.textContent = `{progressNum}% beliveability`
+    progress.textContent = `{progressNum}% beliveability`
   }
 }
