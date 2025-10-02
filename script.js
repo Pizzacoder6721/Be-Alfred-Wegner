4d35ffb496960b4db5272049129b05ce8ad164f3
Pizzacoder6721 <Pizzalover250@hotmail.com>
2025-10-02 19:43:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const progress = document.getElementById("progress");
 const questionNum = 1;
 const choiceBtn1 = document.getElementById("choiceButton1");
 let progressNum;
-
+//Button logic
 choiceBtn1.onclick = function(){
   if(questionNum == 1){
-    progressNum = 1;
+    progressNum += 1;
     progress.textContent = `{progressNum}% beliveability`
   }
 }
+/*Just saying to make sure to make it += when adding progressNum because if you don't we can't check if a question never happened.
+also we should make it so if you get a question wrong you lose a little believability.
