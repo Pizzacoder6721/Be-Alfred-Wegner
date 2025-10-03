32a45a22bb87b6335368025d084af36ed210b782
Pizzacoder6721 <Pizzalover250@hotmail.com>
2025-10-02 20:55:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const progress = document.getElementById("progress");
 const questionNum = 1;
 const choiceBtn1 = document.getElementById("choiceButton1");
-let Number(progressNum) = 0;
+let progressNum = 0;
 //Button logic
 choiceBtn1.onclick = function(){
   if(questionNum == 1){
